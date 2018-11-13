d4d3d0dc231fd14044121e429cb3819af0614b87
zhuzhen1 <994554698@qq.com>
2018-11-13 17:35:26 +0800

    删除动态权限

--- app/src/main/java/com/common/zhuz/activity/MainActivity.java
+++ app/src/main/java/com/common/zhuz/activity/MainActivity.java
@@ -1,14 +1,10 @@
 package com.common.zhuz.activity;
 
-import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
@@ -36,15 +32,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
-import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.OnPermissionDenied;
-import permissions.dispatcher.OnShowRationale;
-import permissions.dispatcher.PermissionRequest;
-import permissions.dispatcher.RuntimePermissions;
 
 import static android.os.Process.killProcess;
 
-@RuntimePermissions
 public class MainActivity extends BaseActivity<MainPresenter> implements MainModelView {
 
     @BindView(R.id.viewpager)
@@ -102,7 +92,6 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainMod
         viewpager.setAdapter(adapter);
 
 
-        MainActivityPermissionsDispatcher.needStorageWithPermissionCheck(this);
 //        mPresenter.postPresener(1,10);
     }
 
@@ -217,53 +206,6 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainMod
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 100) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                needStorage();
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
-    }
-
-    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    public void needStorage() {
-        try {
-            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(),0);
-            String local = pi.versionName;
-            mPresenter.checkUpdate(local);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    public void showRational(final PermissionRequest request) {
-        new AlertDialog.Builder(this)
-                .setTitle("温馨提示")
-                .setMessage("赋予此应用读写文件权限用于版本更新，是否同意?")
-                .setPositiveButton("同意", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        request.proceed();
-                    }
-                })
-                .setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        request.cancel();
-                    }
-                })
-                .show();
-    }
-
-    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    void deniedStorage() {
-        ToastUtil.showToast("请赋予此应用读写文件权限用于版本更新");
     }
 
     /**
